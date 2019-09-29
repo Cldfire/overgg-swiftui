8f1b29e2da21d8d2c23120c14e5b83c2a2d0f123
Jarek Samic <cldfire3@gmail.com>
2019-09-29 09:35:25 -0400

    Initial Commit

---
+++ overgg-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  overgg-swiftui
+//
+//  Created by Jarek Samic on 9/29/19.
+//  Copyright © 2019 Jarek Samic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
