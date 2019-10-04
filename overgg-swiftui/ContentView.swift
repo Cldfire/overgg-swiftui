594989f361a1bd4c78048647289bb0948f2e84f9
Jarek Samic <cldfire3@gmail.com>
2019-10-04 17:37:06 -0400

    Build out a simple UI

--- overgg-swiftui/ContentView.swift
+++ overgg-swiftui/ContentView.swift
@@ -3,16 +3,107 @@
 import SwiftUI
 
 struct ContentView: View {
-    let matches: [MatchBriefInfo] = Downloader().mainPage()!.matchesBrief(.Completed)!
+    let completedMatches: [MatchBriefInfo] = Downloader().mainPage()!.matchesBrief(.Completed)!
+    let upcomingMatches: [MatchBriefInfo] = Downloader().mainPage()!.matchesBrief(.InFuture)!
+    let liveMatches: [MatchBriefInfo] = Downloader().mainPage()!.matchesBrief(.Live)!
     
     var body: some View {
-        VStack {
-            Text("Completed Matches")
-                .font(.title)
+        NavigationView {
+            List {
+                Section(header: Text("Live")) {
+                    ForEach(liveMatches) { match in
+                        Match(match: match, isComplete: false)
+                    }
+                }
+                
+                Section(header: Text("Upcoming")) {
+                    ForEach(upcomingMatches) { match in
+                        UpcomingMatch(match: match)
+                    }
+                }
+                
+                Section(header: Text("Completed")) {
+                    ForEach(completedMatches) { match in
+                        Match(match: match, isComplete: true)
+                    }
+                }
+            }.navigationBarTitle("over.gg app")
+        }
+    }
+}
 
-            ForEach(matches) { match in
-                Text("\(match.teams[0].name) played \(match.teams[1].name)")
+struct Match: View {
+    var match: MatchBriefInfo
+    var isComplete: Bool
+
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("\(match.teams[0].name)")
+                    .if(isComplete && didTeamZeroWin()) { content in
+                        content.bold()
+                    }
+                Text("\(match.teams[1].name)")
+                    .if(isComplete && !didTeamZeroWin()) { content in
+                        content.bold()
+                    }
             }
+            
+            Spacer()
+            
+            VStack {
+                Text("\(match.teams[0].maps_won!)")
+                    .if(isComplete && didTeamZeroWin()) { content in
+                        content.bold()
+                }
+                Text("\(match.teams[1].maps_won!)")
+                    .if(isComplete && !didTeamZeroWin()) { content in
+                        content.bold()
+                    }
+            }
+        }
+    }
+    
+    func didTeamZeroWin() -> Bool {
+        return match.teams[0].maps_won! > match.teams[1].maps_won!
+    }
+}
+
+struct UpcomingMatch: View {
+    var match: MatchBriefInfo
+
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("\(match.teams[0].name)")
+                Text("\(match.teams[1].name)")
+            }
+            
+            Spacer()
+            
+            VStack {
+                Text(formatDate(date: Double(match.scheduled_timestamp!)))
+                    .fontWeight(.light)
+                    .font(.system(size: 11))
+                    .italic()
+            }
+        }
+    }
+
+    func formatDate(date: TimeInterval) -> String {
+        let dformat = DateFormatter()
+        dformat.dateFormat = "MMM d, h:mm a"
+        
+        return dformat.string(from: Date(timeIntervalSince1970: date))
+    }
+}
+
+extension View {
+   func `if`<Content: View>(_ conditional: Bool, content: (Self) -> Content) -> some View {
+        if conditional {
+            return AnyView(content(self))
+        } else {
+            return AnyView(self)
         }
     }
 }
